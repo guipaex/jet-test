166f6417065a9356146c793d4e4754c5cb5a686d
guipaex <guipaex@gmail.com>
2024-02-29 05:00:54 -0300

    :bug: fix: fixed bugs caused by errors in commits

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Table from "./components/Table";
+import db from "./utils/database.json";
 
 export default function Home() {
+  const heads = ['Car', 'Next Reservation', 'Status', 'Rating', 'Actions']
   return (
     <main className="flex min-h-screen max-w-[1440px] items-center justify-center bg-white m-auto">
-      <Table/>
+      <Table data={db} heads={heads}/>
     </main>
   );
 }
