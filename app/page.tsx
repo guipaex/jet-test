ec0f50f0a10601ac1912eb5c68b4db2fe5489de7
guipaex <guipaex@gmail.com>
2024-02-29 07:31:57 -0300

    :sparkles: feat: add mobile table version

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import MobileTable from "./components/MobileTable";
 import Table from "./components/Table";
 import db from "./utils/database.json";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen max-w-[1440px] items-center justify-center bg-white m-auto">
       <Table data={db} heads={heads}/>
+      <MobileTable data={db} heads={heads}/>
     </main>
   );
 }
